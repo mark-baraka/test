753dceddae130ec53e391a7286920eff78f0d168
Mark-Baraka <barakaanderson24@gmail.com>
2022-06-23 19:28:35 +0300

    message

---
+++ jsonExample.py
@@ -0,0 +1,6 @@
+#how to use jsonify for dictionary objects
+from flask import jsonify
+@app.route('/person/')
+def hello():
+    return jsonify({'name':'Jimit',
+                    'address':'Kenya'})
